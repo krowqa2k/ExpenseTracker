f6fb637d03a67c98a5b8d2d13ed60cc44718e652
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-27 19:01:40 +0200

    [App Progress] Added expense rows for ListView

---
+++ ExpanseTracker/Views/TransactionRow.swift
@@ -0,0 +1,35 @@
+//
+//  TransactionRow.swift
+//  ExpanseTracker
+//
+//  Created by admin on 27/06/2024.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    
+    let expense: Expense
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(expense.name)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Text(expense.category)
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                Text(expense.dateParsed, format: .dateTime.year().month().day())
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            Text(expense.amount, format: .currency(code: "PLN"))
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    TransactionRow(expense: Expense(name: "iPhone 15 Pro", amount: 3899.99, category: "Electronic", date: "27/06/2024"))
+}
