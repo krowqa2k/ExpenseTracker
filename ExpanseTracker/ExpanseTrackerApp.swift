3c4e9a38190fbfb33ab75a116dd6a5179276c624
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-27 16:02:55 +0200

    Initial Commit

---
+++ ExpanseTracker/ExpanseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpanseTrackerApp.swift
+//  ExpanseTracker
+//
+//  Created by admin on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpanseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
