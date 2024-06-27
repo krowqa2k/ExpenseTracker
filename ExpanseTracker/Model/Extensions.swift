506faf2575ec91955ca9c70ae378dbf12a5157dc
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-27 18:59:38 +0200

    [App Progress] Added Extension to parse date from string

---
+++ ExpanseTracker/Model/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  ExpanseTracker
+//
+//  Created by admin on 27/06/2024.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let allNumericUSA: DateFormatter = {
+        print("Initializing DateFormatter")
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        
+        return formatter
+    }()
+}
+
+extension String {
+    func dateParsed() -> Date {
+        guard let parsedDate = DateFormatter.allNumericUSA.date(from: self) else { return Date() }
+        
+        return parsedDate
+    }
+}
