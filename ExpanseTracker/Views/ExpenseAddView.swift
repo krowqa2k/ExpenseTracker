74b384e33f010ccec73dbff32dcd8d001fe75ab1
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-27 19:00:52 +0200

    [App Progress] Added AddView

---
+++ ExpanseTracker/Views/ExpenseAddView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpanseAddView.swift
+//  ExpanseTracker
+//
+//  Created by admin on 27/06/2024.
+//
+
+import SwiftUI
+
+struct ExpenseAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseAddView()
+}
